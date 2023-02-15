b33ba2ecfe056d45f1b36c9cc4ef4b195f1ba242
shahao <shahao@zju.edu.cn>
2023-02-15 14:48:13 +0800

    tag_dwa

--- leader_pose_estimate/apriltag_pose.py
+++ leader_pose_estimate/apriltag_pose.py
@@ -2,6 +2,17 @@ import numpy as np
 import apriltag
 import cv2
 import pyrealsense2 as rs
+from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+from scipy.spatial.transform import Rotation as R
+
+
+extrinsic_matrix = np.array([[0,0,1,0],[-1,0,0,0],[0,-1,0,0],[0,0,0,1]]) #相机坐标系:z轴光轴,x右,y下
+tag_len = 0.360
+tag_family = 'tag36h11'
+track_distacne = 2
+
 
 def aprilTag_pose_estimate(img):
     # img = cv2.imread("color.png")
@@ -10,25 +21,29 @@ def aprilTag_pose_estimate(img):
     tags = at_detector.detect(gray)
     #print("tags: {}\n".format(tags))
 
-    cam_params = np.array([612.345825, 613.81781, 318.473251, 237.981806]) #fx, fy, cx, cy
-    tag_len = 48    #tag的边长（单位：mm）
-    obj2world_T = np.array([20, 95, 50])    #物体中心在世界坐标系中的坐标值
+    # cam_params = np.array([612.345825, 613.81781, 318.473251, 237.981806]) #fx, fy, cx, cy
+    # tag_len = 48    #tag的边长（单位：mm）
+    # obj2world_T = np.array([20, 95, 50])    #物体中心在世界坐标系中的坐标值
 
     for tag in tags:       
         if tag.tag_id == 0: #世界坐标系原点为id=10的tag中心
             M, e1, e2 = at_detector.detection_pose(tag, cam_params)
             M[:3,3:] *= tag_len
+            obj2camera = M
 
-            obj2world = np.identity(4)
-            obj2world[:3, 3] = obj2world_T
-            world2camera = M
-            obj2camera = world2camera.dot(obj2world)
-            print("obj2camera:\n", obj2camera)
 
-            for i in range(4):
-                cv2.circle(img, tuple(tag.corners[i].astype(int)), 4, (255, 0, 0), 2)
-            cv2.circle(img, tuple(tag.center.astype(int)), 4, (2, 180, 200), 4)
-            cv2.imwrite("mark.png",img)
+            # obj2world = np.identity(4)
+            # obj2world[:3, 3] = obj2world_T
+            # world2camera = M
+            # obj2camera = world2camera.dot(obj2world)
+            # print("obj2camera:\n", obj2camera)
+
+            # for i in range(4):
+            #     cv2.circle(img, tuple(tag.corners[i].astype(int)), 4, (255, 0, 0), 2)
+            # cv2.circle(img, tuple(tag.center.astype(int)), 4, (2, 180, 200), 4)
+            # cv2.imwrite("mark.png",img)
+
+    return obj2camera
 
 
 def show_colorizer_depth_img(color_image):
@@ -41,16 +56,18 @@ def show_colorizer_depth_img(color_image):
 
 if __name__ == "__main__":
     # Configure depth and color streams
-    a = {"m":1}
-    b= "shahao"
-    for char in b:
-        try:
-            a[char] += 1
-        except:
-            a[char] = 1
-        print(char)
-    # print(a)
-    import pdb;pdb.set_trace()
+    rospy.init_node('tag_pose_estimate', anonymous=True)
+    subgoal_pub = rospy.Publisher('/subgoal',PoseStamped, queue_size=10)
+    # a = {"m":1}
+    # b= "shahao"
+    # for char in b:
+    #     try:
+    #         a[char] += 1
+    #     except:
+    #         a[char] = 1
+    #     print(char)
+    # # print(a)
+    # import pdb;pdb.set_trace()
 
     pipeline = rs.pipeline()
     config = rs.config()
@@ -75,13 +92,32 @@ if __name__ == "__main__":
             depth_image = np.asanyarray(depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
 
-            show_colorizer_depth_img(color_image)
-            aprilTag_pose_estimate(color_image)
+            # show_colorizer_depth_img(color_image)
+            obj2camera = aprilTag_pose_estimate(color_image)
+            camera2robot = extrinsic_matrix
+            subgoal2world = camera2robot.dot(obj2camera)
+            subgoal = PoseStamped()
+            subgoal.header.stamp = rospy.Time.now()
+            subgoal.header.frame_id = "/map"#
+            subgoal.pose.position.x = subgoal2world[0,3]
+            subgoal.pose.position.y = subgoal2world[1,3]
+            subgoal.pose.position.z = subgoal2world[2,3]
+            r3 = R.from_matrix(subgoal2world[:3,:3])
+            qua = r3.as_quat()# (x, y, z, w) format
+            subgoal.pose.orientation.x = qua[0]
+            subgoal.pose.orientation.y = qua[1]
+            subgoal.pose.orientation.z = qua[2]
+            subgoal.pose.orientation.w = qua[3]
+            
+            subgoal_pub.publish(subgoal)
             # Press esc or 'q' to close the image window
-            key = cv2.waitKey(1)
-            if key & 0xFF == ord('q') or key == 27:
-                cv2.destroyAllWindows()
-                break
+            # key = cv2.waitKey(1)
+            # if key & 0xFF == ord('q') or key == 27:
+            #     cv2.destroyAllWindows()
+            #     break
+    
+
+    
     finally:
         # Stop streaming
         pipeline.stop()
--- local_planner_node.py
+++ local_planner_node.py
@@ -8,14 +8,18 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from dynamic_window_approch import DynamicWindowApproch
-    
+
+
 points_cloud = []
 sub_goal = [0,0]
-current_global_yaw = 0
-current_pose_x = 0
-current_pose_y = 0
+# current_global_yaw = 0
+# current_pose_x = 0
+# current_pose_y = 0
+current_pose = [0,0,0]
+current_vel = [0,0]#v,w
 current_linear_speed = 0
 current_angular_speed = 0
+sub_goal = [0,0]
 
 def calc_yaw(orientation):
     """
@@ -27,8 +31,8 @@ def calc_yaw(orientation):
     # print("yaw : ", np.rad2deg(yaw))    
     return yaw
 
-def callback_laser(self, data):        
-    points_cloud = []
+def callback_laser(data):        
+    temp_points_cloud = []
     collision_distance = data.range_max
     angle_min = data.angle_min
     angle_increment = data.angle_increment 
@@ -40,22 +44,39 @@ def callback_laser(self, data):
             continue
         else:
             angle = angle_min + i*angle_increment
-
-            points_cloud.append([np.cos(angle)*data.ranges[i],np.sin(angle)*data.ranges[i]])
+            temp_points_cloud.append([np.cos(angle)*data.ranges[i],np.sin(angle)*data.ranges[i],1])#增广
 
         if (collision_distance > data.ranges[i] > 0):
             collision_distance = data.ranges[i]
+    if len(temp_points_cloud) != 0:
+        temp_points_cloud = np.array(temp_points_cloud)
 
+        robot2world = np.identity(3)
+        robot2world[:2,0] = np.array([np.cos(current_pose[2]),np.sin(current_pose[2])])
+        robot2world[:2,1] = np.array([-np.sin(current_pose[2]),np.cos(current_pose[2])])
+        robot2world[:2,3] = np.array(current_pose[:2])
+        temp_points_cloud = (robot2world.dot(temp_points_cloud.T)).T
+        points_cloud = temp_points_cloud[:,:2].tolist() 
+    else:
+        points_cloud = []
+
+                
     print("points_cloud_num",len(points_cloud))
 
-def callback_odom(self, data):    
+def callback_odom(data):    
+    current_pose[0] = data.pose.pose.position.x
+    current_pose[1] = data.pose.pose.position.y
+    current_pose[2] = calc_yaw(data.pose.pose.orientation)
 
-    current_global_yaw = calc_yaw(data.pose.pose.orientation)
-    current_pose_x = data.pose.pose.position.x
-    current_pose_y = data.pose.pose.position.y
-    current_pose_z = data.pose.pose.position.z
-    current_linear_speed = data.twist.twist.linear.x
-    current_angular_speed = data.twist.twist.angular.z
+    # current_pose_z = data.pose.pose.position.z
+    current_vel[0] = data.twist.twist.linear.x
+    current_vel[1] = data.twist.twist.angular.z
+        
+def callback_subgoal(data):    
+    sub_goal[0] = data.pose.position.x 
+    sub_goal[1] = data.pose.position.y 
+    sub_goal[2] = calc_yaw(data.pose.orientation)
+
         
 
 
@@ -65,7 +86,7 @@ if __name__ == "__main__":
     rospy.init_node('dwa_planner', anonymous=True)
     rospy.Subscriber('/robot_0/scan', LaserScan, callback_laser, queue_size=1)
     rospy.Subscriber('/robot_0/odom', Odometry, callback_odom, queue_size=1)
-    rospy.Subscriber('/robot_0/odom', Odometry, callback_odom, queue_size=1)
+    rospy.Subscriber('/subgoal', PoseStamped, callback_subgoal, queue_size=1)
 
     pub_cmd_vel = rospy.Publisher('/robot_0/cmd_vel', Twist, queue_size=10)
     cmd_vel_value = Twist()
@@ -76,12 +97,11 @@ if __name__ == "__main__":
             max_yawrate=np.radians(40.0),
             base=self.base)
     rospy.spin()
-    rate = rospy.Rate(1000)
+    rate = rospy.Rate(10)
     #try:
     while not rospy.is_shutdown():
-
-        vel_vector = self.Planner.planning(
-                        [current_pose_x,current_pose_y,current_global_yaw], current_linear_speed, self.goal, points_cloud)
+        # vel_vector = self.Planner.planning(current_pose, current_vel, sub_goal[:2], points_cloud)
+        vel_vector = [0.1,0.1]
         cmd_vel_value.linear.x, cmd_vel_value.angular.z = vel_vector[0], vel_vector[1]
         self.pub_cmd_vel.publish(cmd_vel_value)
         rate.sleep()
