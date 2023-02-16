360758819803a62c7afcef8f7f7ab359290ff6fc
shahao <shahao@zju.edu.cn>
2023-02-16 11:15:10 +0800

    finish tag with dwa first week

--- leader_pose_estimate/apriltag_pose.py
+++ leader_pose_estimate/apriltag_pose.py
@@ -2,6 +2,7 @@ import numpy as np
 import apriltag
 import cv2
 import pyrealsense2 as rs
+import rospy
 from sensor_msgs.msg import Image
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
@@ -9,15 +10,17 @@ from scipy.spatial.transform import Rotation as R
 
 
 extrinsic_matrix = np.array([[0,0,1,0],[-1,0,0,0],[0,-1,0,0],[0,0,0,1]]) #相机坐标系:z轴光轴,x右,y下
+cam_params = np.array([554.254691191187, 554.254691191187, 320.5, 240.5]) 
 tag_len = 0.360
 tag_family = 'tag36h11'
 track_distacne = 2
 
 
 def aprilTag_pose_estimate(img):
+    global subgoal_pub
     # img = cv2.imread("color.png")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    at_detector = apriltag.Detector(apriltag.DetectorOptions(families='tag16h5'))
+    at_detector = apriltag.Detector(apriltag.DetectorOptions(families=tag_family))
     tags = at_detector.detect(gray)
     #print("tags: {}\n".format(tags))
 
@@ -42,8 +45,25 @@ def aprilTag_pose_estimate(img):
             #     cv2.circle(img, tuple(tag.corners[i].astype(int)), 4, (255, 0, 0), 2)
             # cv2.circle(img, tuple(tag.center.astype(int)), 4, (2, 180, 200), 4)
             # cv2.imwrite("mark.png",img)
+            camera2robot = extrinsic_matrix
+            subgoal2world = camera2robot.dot(obj2camera)
+            subgoal = PoseStamped()
+            subgoal.header.stamp = rospy.Time.now()
+            subgoal.header.frame_id = "map"#
+            subgoal.pose.position.x = subgoal2world[0,3]
+            subgoal.pose.position.y = subgoal2world[1,3]
+            subgoal.pose.position.z = subgoal2world[2,3]
+            r3 = R.from_matrix(subgoal2world[:3,:3])
+            qua = r3.as_quat()# (x, y, z, w) format
+            subgoal.pose.orientation.x = qua[0]
+            subgoal.pose.orientation.y = qua[1]
+            subgoal.pose.orientation.z = qua[2]
+            subgoal.pose.orientation.w = qua[3]
+            
+            subgoal_pub.publish(subgoal)
+            print("obj2camera:\n", obj2camera)
 
-    return obj2camera
+
 
 
 def show_colorizer_depth_img(color_image):
@@ -93,23 +113,8 @@ if __name__ == "__main__":
             color_image = np.asanyarray(color_frame.get_data())
 
             # show_colorizer_depth_img(color_image)
-            obj2camera = aprilTag_pose_estimate(color_image)
-            camera2robot = extrinsic_matrix
-            subgoal2world = camera2robot.dot(obj2camera)
-            subgoal = PoseStamped()
-            subgoal.header.stamp = rospy.Time.now()
-            subgoal.header.frame_id = "/map"#
-            subgoal.pose.position.x = subgoal2world[0,3]
-            subgoal.pose.position.y = subgoal2world[1,3]
-            subgoal.pose.position.z = subgoal2world[2,3]
-            r3 = R.from_matrix(subgoal2world[:3,:3])
-            qua = r3.as_quat()# (x, y, z, w) format
-            subgoal.pose.orientation.x = qua[0]
-            subgoal.pose.orientation.y = qua[1]
-            subgoal.pose.orientation.z = qua[2]
-            subgoal.pose.orientation.w = qua[3]
-            
-            subgoal_pub.publish(subgoal)
+            aprilTag_pose_estimate(color_image)
+
             # Press esc or 'q' to close the image window
             # key = cv2.waitKey(1)
             # if key & 0xFF == ord('q') or key == 27:
